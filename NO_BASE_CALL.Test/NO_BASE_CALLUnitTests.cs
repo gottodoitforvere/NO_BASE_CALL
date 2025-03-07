6bbd8fbe01d76cec84a909975688de17cfb98360
yshch <yshch@YSS_HMB16>
2025-03-07 17:24:43 +0300

    Добавьте файлы проекта.

---
+++ NO_BASE_CALL.Test/NO_BASE_CALLUnitTests.cs
@@ -0,0 +1,239 @@
+﻿using Microsoft;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+using VerifyCS = NO_BASE_CALL.Test.CSharpCodeFixVerifier<
+    NO_BASE_CALL.NO_BASE_CALLAnalyzer,
+    NO_BASE_CALL.NO_BASE_CALLCodeFixProvider>;
+
+namespace NO_BASE_CALL.Test
+{
+    [TestClass]
+    public class NO_BASE_CALLUnitTest
+    {
+        [TestMethod]
+        public async Task TestMethod1()
+        {
+            var test = @"public class A 
+{
+   public virtual void Foo() {}
+}
+public class B1 : A 
+{ 
+   public override void Foo()
+   { 
+      base.Foo(); 
+   } 
+}
+public class B2 : A 
+{ 
+   public override void Foo()  
+   { 
+      ;
+   } 
+}";
+
+            await VerifyCS.VerifyAnalyzerAsync(test);
+        }
+
+        
+        [TestMethod]
+        public async Task TestMethod2()
+        {
+            var test = @"
+public class A 
+{
+   public virtual void Foo() {}
+}
+public class B1 : A 
+{ 
+   public override void Foo() 
+   { 
+      base.Foo(); 
+   } 
+}
+public class B2 : A 
+{ 
+   public override void Foo()  
+   { 
+      base.Foo(); 
+   } 
+}
+public class B3 : A 
+{ 
+   public void Foo()  
+   { 
+      ;
+   } 
+}";
+
+            await VerifyCS.VerifyAnalyzerAsync(test);
+        }
+        [TestMethod]
+        public async Task TestMethod3()
+        {
+            var test = @"
+public class A 
+{
+   public virtual void Foo() {}
+}
+public class B1 : A 
+{ 
+   public void Foo() 
+   { 
+      base.Foo(); 
+   } 
+}
+public class B2 : A 
+{ 
+   public void Foo()  
+   { 
+      ; 
+   } 
+}
+public class B3 : A 
+{ 
+   public void Foo()  
+   { 
+      ;
+   } 
+}";
+
+
+            await VerifyCS.VerifyAnalyzerAsync(test);
+        }
+
+        [TestMethod]
+        public async Task TestMethod4()
+        {
+            string testCode = @"
+public class A 
+{
+   public virtual void Foo() {}
+}
+public class B1 : A 
+{ 
+   public override void Foo() 
+   { 
+      base.Foo(); 
+   } 
+}
+public class B2 : A 
+{ 
+   public override void Foo()  
+   { 
+      base.Foo(); 
+   } 
+}
+public class B3 : A 
+{ 
+   public override void Foo()  
+   { 
+      ;
+   } 
+}";
+            var expected = VerifyCS.Diagnostic().WithSpan(22, 25, 22, 28).WithMessage("The implementation of method Foo() doesn't call corresponding method Foo() from the base class, but usually (more than 65% cases) it does it (2 out of 3 times).");
+            await VerifyCS.VerifyAnalyzerAsync(testCode, expected);
+        }
+        [TestMethod]
+        public async Task TestMethod5()
+        {
+            string testCode = @"
+public class A 
+{
+   public virtual void Foo() {}
+}
+public class B1 : A 
+{ 
+   public override void Foo() 
+   { 
+      base.Foo(); 
+   } 
+}
+public class B2 : A 
+{ 
+   public override void Foo()  
+   { 
+      base.Foo(); 
+   } 
+}
+public class B3 : A 
+{ 
+   public override void Foo()  
+   { 
+      ;
+   } 
+}
+public class B4 : A 
+{ 
+   public override void Foo()  
+   { 
+    int x = 0;
+    for (int i = 0; i < 27; i++) base.Foo(); 
+   } 
+}";
+
+            var expected = VerifyCS.Diagnostic().WithSpan(22, 25, 22, 28).WithMessage("The implementation of method Foo() doesn't call corresponding method Foo() from the base class, but usually (more than 65% cases) it does it (3 out of 4 times).");
+            await VerifyCS.VerifyAnalyzerAsync(testCode, expected);
+        }
+        [TestMethod]
+        public async Task TestMethod6()
+        {
+            string testCode = @"
+public class A
+{
+    public virtual void Foo() { } 
+}
+public class B1 : A
+{
+    public override void Foo()  
+    {
+        base.Foo();
+    }
+}
+public class B2 : A
+{
+    public override void Foo()  
+    {
+        base.Foo();
+    }
+}
+public class B5 : A
+{
+    public override void Foo()  
+    {
+        base.Foo();
+    }
+}
+public class B6 : A
+{
+    public override void Foo() 
+    {
+        base.Foo();
+    }
+}
+public class B3 : A
+{
+    public override void Foo() 
+    {
+        ;
+    }
+}
+public class B4 : A
+{
+    public override void Foo()      
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            ;
+        }
+    }
+}";
+
+            var expected_1 = VerifyCS.Diagnostic().WithSpan(36, 26, 36, 29).WithMessage("The implementation of method Foo() doesn't call corresponding method Foo() from the base class, but usually (more than 65% cases) it does it (4 out of 6 times).");
+            var expected_2 = VerifyCS.Diagnostic().WithSpan(43, 26, 43, 29).WithMessage("The implementation of method Foo() doesn't call corresponding method Foo() from the base class, but usually (more than 65% cases) it does it (4 out of 6 times).");
+            await VerifyCS.VerifyAnalyzerAsync(testCode, expected_1, expected_2);
+        }
+
+    }
+
+}
---
+++ NO_BASE_CALL/NO_BASE_CALLAnalyzer.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace NO_BASE_CALL
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class NO_BASE_CALLAnalyzer : DiagnosticAnalyzer
+    {
+        const int Threshold = 65;
+        public const string DiagnosticId = "NO_BASE_CALL_STAT";
+        private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.AnalyzerTitle), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
+        private const string Category = "Naming";
+        private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.EnableConcurrentExecution();
+            context.RegisterSymbolAction(AnalyzeSymbol, SymbolKind.Method);
+        }
+
+        private static void AnalyzeSymbol(SymbolAnalysisContext context)
+        {
+            IMethodSymbol method = (IMethodSymbol) context.Symbol;
+            if (!method.IsOverride)
+            {
+                return;
+            }
+
+            var basemethod = method.OverriddenMethod;
+            if (basemethod == null) return;
+
+            //Поиск всех override базового метода
+            var overrides = new List<IMethodSymbol>();
+            foreach (var method_i in context.Compilation.GetSymbolsWithName(basemethod.Name, SymbolFilter.Member))
+            {
+                IMethodSymbol candidate = (IMethodSymbol) method_i;
+                if (candidate.OverriddenMethod != null && candidate.OverriddenMethod.Equals(basemethod, SymbolEqualityComparer.Default)) overrides.Add(candidate);
+            }
+
+            // Подсчет вызовов базового метода среди override + устанавливаем данный факт для текущего метода
+            int base_call_count = 0;
+            bool current_method_calls_base = false;
+            foreach (var method_i in overrides)
+            {
+                var method_declaration = method_i.DeclaringSyntaxReferences.FirstOrDefault()?.GetSyntax() as MethodDeclarationSyntax;
+                bool this_is_current_method = SymbolEqualityComparer.Default.Equals(method, method_i);
+                foreach (var node in method_declaration.Body.DescendantNodes())
+                {
+                    if (node is BaseExpressionSyntax)
+                    {
+                        base_call_count++;
+                        if (this_is_current_method) current_method_calls_base = true;
+                        break; 
+                    }
+                }
+            }
+
+            if (!current_method_calls_base && (base_call_count * 100 > Threshold * overrides.Count))
+            {
+                var message = string.Format(MessageFormat.ToString(), method.Name, basemethod.Name, Threshold, base_call_count, overrides.Count);
+                var diagnostic = Diagnostic.Create(Rule, method.Locations[0], method.Name,basemethod.Name, Threshold, base_call_count, overrides.Count());
+                context.ReportDiagnostic(diagnostic);
+            }
+        }
+    }
+}
